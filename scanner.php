24b9f7cf1beac8d164e002acee0d6463eaa14ddb
Lorenzo Milesi <maxxer@yetopen.it>
2015-03-16 08:23:12 +0100

    Scanner da mettere in cron

---
+++ scanner.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * Radio Deejay RELOADED podcaster
+ * @author Lorenzo Milesi <lorenzo@mile.si>
+ * @copyright 2015 Lorenzo Milesi 
+ * @license GNU GPL v3
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+require_once 'rdjreloaded.php';
+
+$lib = new RDJReloaded();
+$lib->scanList();
+$lib->aggiornaPodcast();
